676e10a5927d9dde86584e0458c39e6c507247ea
residencias <sistemawebmonitoreotec@gmail.com>
2026-01-27 21:45:35 -0600

    Guards

--- src/app/auth/guards/admin.guard.ts
+++ src/app/auth/guards/admin.guard.ts
@@ -1,31 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { AuthService } from '../services/auth/auth.service';
-import { CookieService } from 'ngx-cookie-service';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  
-  const auth = inject(AuthService);
+export const teacherGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
   const router = inject(Router);
-  const cookies = inject(CookieService);
 
-  return auth.userAccount().pipe(
-    map(userType => {
-      if(userType === 'Administrador'){
-        return true;
-      }else{
-        cookies.delete('session');
-        setTimeout(() => {
-          window.location.reload();
-          router.navigate(['/iniciar-sesion']).then(() => {
-            window.location.reload();
-          });
-        }, 1);
-        return false;
+  return new Observable<boolean>((observer) => {
+    authService.userAccount().subscribe((userType) => {
+      if (userType !== 'Administrador') {
+        router.navigate(['/auth/login']);
+        observer.next(false);
+      } else {
+        observer.next(true);
       }
-    })
-  );
-
+      observer.complete();
+    });
+  });
 };
--- src/app/auth/guards/teacher.guard.ts
+++ src/app/auth/guards/teacher.guard.ts
@@ -1,31 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { map } from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from '../services/auth/auth.service';
 
 export const teacherGuard: CanActivateFn = (route, state) => {
-  
-  const auth = inject(AuthService);
+  const authService = inject(AuthService);
   const router = inject(Router);
-  const cookies = inject(CookieService);
 
-  return auth.userAccount().pipe(
-    map(userType => {
-      if(userType === 'Docente'){
-        return true;
-      }else{
-        cookies.delete('session');
-        setTimeout(() => {
-          window.location.reload();
-          router.navigate(['/iniciar-sesion']).then(() => {
-            window.location.reload();
-          });
-        }, 1);
-        return false;
+  return new Observable<boolean>((observer) => {
+    authService.userAccount().subscribe((userType) => {
+      if (userType !== 'Docente') {
+        router.navigate(['/auth/login']);
+        observer.next(false);
+      } else {
+        observer.next(true);
       }
-    })
-  );
-
+      observer.complete();
+    });
+  });
 };
